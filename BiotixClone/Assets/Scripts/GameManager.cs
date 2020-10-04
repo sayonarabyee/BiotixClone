ca723b3705b38c0061332d670a290361ba06f30a
SashaV <sasha1404568@gmail.com>
2020-10-04 20:45:37 +0300

    update

---
+++ BiotixClone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isWin;
+    private void Start()
+    {
+        // Создать лист со всеми элементами и проверять тэг
+        // Если тэг совпадает у всех элементов - заканчивать уровень
+        InvokeRepeating("CheckWin", 0f, 1.5f);
+    }
+    void CheckWin()
+    {
+	
+    }
+}
